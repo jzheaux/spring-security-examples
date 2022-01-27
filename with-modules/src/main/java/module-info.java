336109fd9b6a497d9fcc381179502a5ccba6ff74
Josh Cummings <josh.cummings@gmail.com>
2022-01-27 10:48:08 -0700

    Add missing modules

--- with-modules/src/main/java/module-info.java
+++ with-modules/src/main/java/module-info.java
@@ -1,10 +1,12 @@
-module com.example.withmodules {
+open module com.example.withmodules {
 	requires org.opensaml.core;
 
 	requires spring.boot;
 	requires spring.boot.autoconfigure;
 
 	requires spring.context;
+	requires spring.core;
+	requires spring.beans;
 	requires spring.security.core;
 	requires spring.security.saml2.service.provider;
 	requires spring.web;
