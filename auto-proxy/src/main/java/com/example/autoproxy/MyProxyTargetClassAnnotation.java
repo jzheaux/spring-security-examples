9be5af69cc89b194f6c744deee5d616d01ad201a
Josh Cummings <josh.cummings@gmail.com>
2024-07-26 12:57:40 -0600

    Retain AutoProxy-configuring annotation

--- auto-proxy/src/main/java/com/example/autoproxy/MyProxyTargetClassAnnotation.java
+++ auto-proxy/src/main/java/com/example/autoproxy/MyProxyTargetClassAnnotation.java
@@ -1,7 +1,14 @@
 package com.example.autoproxy;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.springframework.context.annotation.AdviceMode;
 
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MyProxyTargetClassAnnotation {
 	AdviceMode mode() default AdviceMode.PROXY;
 	boolean proxyTargetClass() default false;
